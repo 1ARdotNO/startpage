223257a0e6ed4fa359dbae7cba7798a52c135b4c
Einar Stenberg <einar@stenberg.im>
2021-11-02 14:12:33 +0100

    Update app.js

--- app.js
+++ app.js
@@ -75,9 +75,9 @@ const updateDate = () => {
   let currentYear = completeDate.getFullYear();
 
   // Update the Time
-  currentTime.innerHTML = `${
-    currentHour % 12 == 0 ? "12" : currentHour % 12
-  }:${currentMinute} ${currentHour > 11 ? "PM" : "AM"}`;
+  //currentTime.innerHTML = `${
+  //  currentHour % 12 == 0 ? "12" : currentHour % 12
+  //}:${currentMinute} ${currentHour > 11 ? "PM" : "AM"}`;
 
   // Update the Date
   currentDate.innerHTML = `${DAYS[currentDay]} ${currentNumber}, ${MONTHS[currentMonth]} ${currentYear}`;
